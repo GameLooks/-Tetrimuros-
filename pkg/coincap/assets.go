aa45b0a5610d16e4d4d5dde59b050b85e44955c9
GameLooks <99416873+GameLooks@users.noreply.github.com>
2020-09-14 09:27:35 +0000

    authentification

--- pkg/coincap/assets.go
+++ pkg/coincap/assets.go
@@ -9,4 +9,4 @@ import (
 // AssetsRequest contains the paramaters for modifying a query to
 // the "/assets" endpoint. Search can be a symbol (BTC) or an asset id (bitcoin)
 type AssetsRequest struct {
-	Search string `json:"search,omitempty"
+	Search string `json:"search,omitempty"` // search by asset id (bitcoin) or s
