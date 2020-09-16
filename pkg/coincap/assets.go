5d4fdcf064449b3309e556c0e7557cfedf90cb88
GameLooks <99416873+GameLooks@users.noreply.github.com>
2020-09-16 16:12:04 +0000

    heroku-automated-project-analysis (adding libcsv v0.9.0)

--- pkg/coincap/assets.go
+++ pkg/coincap/assets.go
@@ -9,4 +9,5 @@ import (
 // AssetsRequest contains the paramaters for modifying a query to
 // the "/assets" endpoint. Search can be a symbol (BTC) or an asset id (bitcoin)
 type AssetsRequest struct {
-	Search string `json:"search,omitempty"` // search by asset id (bitcoin) or s
+	Search string `json:"search,omitempty"` // search by asset id (bitcoin) or symbol (BTC)
+	Limit  int    `json:"limi
