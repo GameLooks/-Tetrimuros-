b02f86b142c8718ab1fff959030c496b193824ed
GameLooks <99416873+GameLooks@users.noreply.github.com>
2020-09-18 22:22:43 +0000

    [DOC] Working on matrix doc

--- pkg/coincap/assets.go
+++ pkg/coincap/assets.go
@@ -11,4 +11,4 @@ import (
 type AssetsRequest struct {
 	Search string `json:"search,omitempty"` // search by asset id (bitcoin) or symbol (BTC)
 	Limit  int    `json:"limit,omitempty"`  // limit number of returned results (Max: 2000)
-	Offset int    `json:"offset,omitem
+	Offset int    `json:"offset,omitempty"` // skip the first N entries of t
