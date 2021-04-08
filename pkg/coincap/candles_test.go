1f1dcee633e7e03473f7feb7cc4dff6e4e1b3755
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-04-08 13:03:53 +0000

    Code clean-up

--- pkg/coincap/candles_test.go
+++ pkg/coincap/candles_test.go
@@ -12,4 +12,4 @@ func TestCandles(t *testing.T) {
 	r.HandleFunc("/candles", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-	
+		fmt.Fprint(w, fixture("candl
