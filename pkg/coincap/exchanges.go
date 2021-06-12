9a3dde09155d1eaefefdd8bb81dbb8844f445435
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-06-12 14:35:04 +0000

    updated locale files

--- pkg/coincap/exchanges.go
+++ pkg/coincap/exchanges.go
@@ -6,4 +6,5 @@ import (
 )
 
 // Exchange contains information about a cryptocurrency exchange. This includes the exchanges
-// relative rank, volume, and whether tr
+// relative rank, volume, and whether trading sockets are available
+type Exchange str
