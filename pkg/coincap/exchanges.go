890acbb10790eaeddf2b67689c3fe7229b62e7dd
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-06-19 13:34:46 +0000

    Update samples

--- pkg/coincap/exchanges.go
+++ pkg/coincap/exchanges.go
@@ -8,4 +8,4 @@ import (
 // Exchange contains information about a cryptocurrency exchange. This includes the exchanges
 // relative rank, volume, and whether trading sockets are available
 type Exchange struct {
-	ID                 string    `json:"id"`            
+	ID                 string    `json:"id"`                 // unique identifier for ex
