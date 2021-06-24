181a9680510e9c8fc70908eaa02cfcdf2e3ed995
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-06-24 20:16:03 +0000

    Add rule

--- pkg/coincap/exchanges.go
+++ pkg/coincap/exchanges.go
@@ -10,4 +10,5 @@ import (
 type Exchange struct {
 	ID                 string    `json:"id"`                 // unique identifier for exchange
 	Name               string    `json:"name"`               // proper name of exchange
-	Rank               string    `json:"rank"`         
+	Rank               string    `json:"rank"`               // rank in terms of total volume compared to other exchanges
+	P
