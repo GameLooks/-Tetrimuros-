3b9322d159aa281ad313df66d784411e9e393b55
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-07-10 11:15:14 +0000

    fixed naming

--- pkg/coincap/exchanges.go
+++ pkg/coincap/exchanges.go
@@ -12,4 +12,5 @@ type Exchange struct {
 	Name               string    `json:"name"`               // proper name of exchange
 	Rank               string    `json:"rank"`               // rank in terms of total volume compared to other exchanges
 	PercentTotalVolume string    `json:"percentTotalVolume"` // perecent of total daily volume in relation to all exchanges
-	VolumeUSD          string    `json:"volumeUSD"`          //
+	VolumeUSD          string    `json:"volumeUSD"`          // daily volume represented in USD
+	TradingP
