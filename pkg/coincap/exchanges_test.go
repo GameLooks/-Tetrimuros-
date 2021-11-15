758b8bf32149ce0dc82d92cad3969a421f17c5f1
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-11-15 22:51:57 +0000

    Revert "serial: 8250_pci: add support for another kind of NetMos Technology PCI 9835 Multi-I/O Controller"

--- pkg/coincap/exchanges_test.go
+++ pkg/coincap/exchanges_test.go
@@ -68,4 +68,5 @@ func TestExchangeByID(t *testing.T) {
 	defer teardown()
 
 	r.HandleFunc("/exchanges/{id}", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(
