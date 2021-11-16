7f771b18c0896ee5734b42bfcc72eec67fdcb24c
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-11-16 08:41:47 +0000

    add progress bar

--- pkg/coincap/exchanges_test.go
+++ pkg/coincap/exchanges_test.go
@@ -69,4 +69,5 @@ func TestExchangeByID(t *testing.T) {
 
 	r.HandleFunc("/exchanges/{id}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprin
