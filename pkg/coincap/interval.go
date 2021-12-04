f21db6938578e61961000911f80c09a995ad5e53
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-12-04 22:25:32 +0000

    Ajax

--- pkg/coincap/interval.go
+++ pkg/coincap/interval.go
@@ -3,4 +3,5 @@ package coincap
 // Interval represents point-in-time intervals for retrieving historical market data
 type Interval string
 
-// Valid Intervals for histori
+// Valid Intervals for historical market data
+// Used when requesting As
