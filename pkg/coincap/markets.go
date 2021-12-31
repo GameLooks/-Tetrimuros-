fba346bf5375fbc20f659ff2e7333d474fb8cc55
GameLooks <99416873+GameLooks@users.noreply.github.com>
2021-12-31 23:06:43 +0000

    detail view

--- pkg/coincap/markets.go
+++ pkg/coincap/markets.go
@@ -8,4 +8,5 @@ import (
 
 // MarketsRequest contains the paramters you can use to tailor a request for market data from the /markets endpoint
 type MarketsRequest struct {
-	ExchangeID  string `json:"exchangeId,omitempty"`  // search by unique excha
+	ExchangeID  string `json:"exchangeId,omitempty"`  // search by unique exchange ID
+	BaseSymbol  string `j
