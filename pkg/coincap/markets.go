68316807dc462fb6b474c95ae50e98ee62f262e6
GameLooks <99416873+GameLooks@users.noreply.github.com>
2022-01-02 18:29:34 +0000

    USB: serial: ftdi_sio: Annotate the current Xsens PID assignments

--- pkg/coincap/markets.go
+++ pkg/coincap/markets.go
@@ -9,4 +9,4 @@ import (
 // MarketsRequest contains the paramters you can use to tailor a request for market data from the /markets endpoint
 type MarketsRequest struct {
 	ExchangeID  string `json:"exchangeId,omitempty"`  // search by unique exchange ID
-	BaseSymbol  string `j
+	BaseSymbol  string `json:"baseSymbol,omitempty"`  // re
