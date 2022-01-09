2e76a6017a416adf775bc5ba6d0e65c04de353a9
GameLooks <99416873+GameLooks@users.noreply.github.com>
2022-01-09 19:53:34 +0000

    Make it compile.

--- pkg/coincap/markets.go
+++ pkg/coincap/markets.go
@@ -11,4 +11,5 @@ type MarketsRequest struct {
 	ExchangeID  string `json:"exchangeId,omitempty"`  // search by unique exchange ID
 	BaseSymbol  string `json:"baseSymbol,omitempty"`  // return all results with this base symbol
 	BaseID      string `json:"baseId,omitempty"`      // return all results with this base id
-	QuoteSymbol string `json:"quoteSymbol,omitempty"` // return all results with this quote s
+	QuoteSymbol string `json:"quoteSymbol,omitempty"` // return all results with this quote symbol
+	QuoteID     string `json:"quote
