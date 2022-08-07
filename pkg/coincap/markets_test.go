df1a94637e473021a8ee941ac0e6f83245c55445
GameLooks <99416873+GameLooks@users.noreply.github.com>
2022-08-07 15:00:10 +0000

    added username

--- pkg/coincap/markets_test.go
+++ pkg/coincap/markets_test.go
@@ -15,3 +15,34 @@ func TestMarkets(t *testing.T) {
 	r.HandleFunc("/markets", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, fixture("markets.json"))
+	})
+
+	params := &MarketsRequest{
+		ExchangeID:  "binance",
+		BaseSymbol:  "ETH",
+		QuoteSymbol: "BTC",
+		Limit:       100,
+		Offset:      0,
+	}
+
+	markets, _, err := client.Markets(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(markets) < 0 {
+		t.Fatalf("No Markets returned")
+	}
+	got := markets[0]
+	if got.ExchangeID != "binance" {
+		t.Errorf("Expected exchangeID to be binance but was %s", got.ExchangeID)
+	}
+}
+
+func TestMarketsLive(t *testing.T) {
+	// Hit the actual API
+	client := NewClient(nil)
+
+	params := &MarketsRequest{
+		BaseID:      "ethereum",
+		QuoteID:     "dogecoin",
