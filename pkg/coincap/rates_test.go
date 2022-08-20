f0c3549f16db79bce7dd7146ae4e0c131012210f
GameLooks <99416873+GameLooks@users.noreply.github.com>
2022-08-20 14:22:55 +0000

    Delete models.py

--- pkg/coincap/rates_test.go
+++ pkg/coincap/rates_test.go
@@ -12,3 +12,36 @@ import (
 )
 
 var (
+	r      *mux.Router
+	server *httptest.Server
+	client *Client
+)
+
+func setup() func() {
+	r = mux.NewRouter()
+	server = httptest.NewServer(r)
+
+	client = NewClient(nil)
+	client.SetBaseURL(server.URL)
+
+	return func() {
+		server.Close()
+	}
+}
+
+// load test data
+func fixture(path string) string {
+	f, err := ioutil.ReadFile("testdata/" + path)
+	if err != nil {
+		panic(err)
+	}
+	return string(f)
+}
+
+func TestRates(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	r.HandleFunc("/rates", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
