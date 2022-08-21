891f8454e6adaaf744c54073c4363ed5446d2995
GameLooks <99416873+GameLooks@users.noreply.github.com>
2022-08-21 08:00:49 +0000

    fix javascript

--- pkg/coincap/rates_test.go
+++ pkg/coincap/rates_test.go
@@ -45,3 +45,44 @@ func TestRates(t *testing.T) {
 	r.HandleFunc("/rates", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, fixture("rates.json"))
+	})
+
+	rates, _, err := client.Rates()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(rates) < 0 {
+		t.Fatalf("No rates returned")
+	}
+	got := rates[0]
+	expected := Rate{
+		ID:             "romanian-leu",
+		Symbol:         "RON",
+		CurrencySymbol: "lei",
+		Type:           "fiat",
+		RateUSD:        "0.2505529076289101",
+	}
+	if *got != expected {
+		t.Errorf("Expected %s, Got %s", expected, got)
+	}
+}
+
+func TestRatesBadURL(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	client.SetBaseURL("bad.fail:")
+	_, _, err := client.Rates()
+	if err == nil {
+		t.Errorf("Expected client to fail on Rates() because of bad base path")
+	}
+	_, _, err = client.RateByID("bitcoin")
+	if err == nil {
+		t.Errorf("Expected client to fail on RateByID() because of bad base path")
+	}
+
+}
+
+func TestRateByID(t *testing.T) {
+	teardown := setup()
